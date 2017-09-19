8b4e5248b7a7c04572d51527788c8990de4dc2e3
Anna Ulanova <anetteua0@gmail.com>
2017-09-19 14:57:40 -0700

    s

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
 var express = require('express');
 var app = express();
+
+app.get('', function(req, res){
+  res.send('hello world');
+});
+
+app.listen('3000');
+
+console.log('Port 3000 is up');
+exports = module.exports = app;
